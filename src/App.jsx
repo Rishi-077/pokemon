cc4d65cfb347d856ae41a7e807619645045b590f
Rishi-077 <rishikesan070@gmail.com>
2024-02-11 21:36:05 +0530

    page route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import PageRoute from "./pages/PageRoute/PageRoute";
 import Login from "./pages/Login/Login";
-import NotFound from "./pages/NotFount/NotFound";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route element={<NotFound />} />
+          <Route path="/*" element={<PageRoute />} />
           <Route path="/" element={<Navigate replace to="/login" />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<PageRoute />} />
+          <Route element={<NotFound />} />
         </Routes>
       </div>
     </>
